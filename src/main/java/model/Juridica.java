d6092de98e38fbc3ccb55747be4b7e79f6a48588
matheusmacias <matheusmacias3@gmail.com>
2022-11-05 02:44:46 -0300

    v2

--- src/main/java/model/Juridica.java
+++ src/main/java/model/Juridica.java
@@ -1,5 +1,13 @@
 package model;
 
 public class Juridica extends Pessoa{
+    private String cnpj;
 
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
 }
